20ea1b08eea0cca752417f6deec914fa5f37fb75
Frans <franssiswanto@gmail.com>
2016-12-06 09:52:00 +0700

    kaskus only converter engine

--- lib/object/source.py
+++ lib/object/source.py
@@ -1,14 +1,15 @@
 from .database        import Database
 from .crawler         import Crawler
 import pymongo
+import re
 
 class Source:
 	def __init__(self):
 		conn = pymongo.MongoClient("mongodb://mongo:27017/monitor")
 		db   = conn["monitor"]
-		
+
 		self.crawlers = []
-		for doc in db.crawlers_meta.find():
+		for doc in db.crawlers_meta.find({"name": re.compile("Kaskus Crawler", re.IGNORECASE)}):
 			crawler = Crawler(
 				name = doc["name"],
 				type = doc["type"],
--- run.py
+++ run.py
@@ -23,35 +23,37 @@ def run_converter(crawler):
 		number_of_document = 0
 	print("[ConverterEngine][debug] Found %s document(s) in %s" % (number_of_document, crawler.name))
 
+	with multiprocessing.Pool(10) as pool:
+		pool.map(batch_convert, docs)
+		
+def batch_convert(doc):
 	mention = None
-	for doc in docs:
-		try:
-			parser  = ParserFactory.get_parser(ParserFactory.RAW_MENTION)
-			mention = parser.parse(crawler, doc)
+	try:
+		parser  = ParserFactory.get_parser(ParserFactory.RAW_MENTION)
+		mention = parser.parse(crawler, doc)
 
-			parser = ParserFactory.get_parser(ParserFactory.AUTHOR_INFO)
-			author = parser.parse(mention)
+		parser = ParserFactory.get_parser(ParserFactory.AUTHOR_INFO)
+		author = parser.parse(mention)
 
-			mention_saver = SaverFactory.get_saver(SaverFactory.MENTION)
-			mention_saver.save(mention)
+		mention_saver = SaverFactory.get_saver(SaverFactory.MENTION)
+		mention_saver.save(mention)
 
-			author_saver = SaverFactory.get_saver(SaverFactory.AUTHOR_INFO)
-			author_saver.save(author)
+		author_saver = SaverFactory.get_saver(SaverFactory.AUTHOR_INFO)
+		author_saver.save(author)
 
+		mention_saver.set_as_converted(crawler, mention)
+		print(fmtstr("[%s][success] Converted One Document!" % crawler.name, "green"))
+	except ValidationError as ex:
+		logger.error(str(ex), exc_info=True)
+	except DuplicateMention as ex:
+		if mention is not None:
 			mention_saver.set_as_converted(crawler, mention)
-			print(fmtstr("[%s][success] Converted One Document!" % crawler.name, "green"))
-		except ValidationError as ex:
-			logger.error(str(ex), exc_info=True)
-		except DuplicateMention as ex:
-			if mention is not None:
-				mention_saver.set_as_converted(crawler, mention)
-			print(fmtstr("[%s][success] Ops! Duplicate document" % crawler.name, "red"))
-		except NetworkTimeout as ex:
-			logger.error(str(ex), exc_info=True)
+		print(fmtstr("[%s][success] Ops! Duplicate document" % crawler.name, "red"))
+	except NetworkTimeout as ex:
+		logger.error(str(ex), exc_info=True)
 
 if __name__ == "__main__":
 	Logger()
 	source = Source()
-	with multiprocessing.Pool(20) as pool:
-		pool.map(run_converter, source.crawlers)
+	run_converter(source.crawlers[0])
 		
